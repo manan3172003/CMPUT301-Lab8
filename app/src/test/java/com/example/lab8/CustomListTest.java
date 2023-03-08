88f83a04a9f6fe1cf42adf988b9a60d0c08ab240
manan3172003 <mjain2@ualberta.ca>
2023-03-07 18:01:31 -0700

    making testDeleteCity pass

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -28,4 +28,15 @@ public class CustomListTest {
         list.addCity(city);
         assertEquals(true, list.hasCity(city));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList(null, citylist);
+        City city = new City("Edmonton", "Alberta");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+        list.deleteCity(city);
+        assertEquals(0, list.getCount());
+    }
+
 }
